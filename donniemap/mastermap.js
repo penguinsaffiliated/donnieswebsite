153d148f8443a66e5747d515677da66b3eb023f2
Deividas Nikliauza <d.nikliauza@gmail.com>
2019-08-05 09:55:29 -0600

    simple article template

--- donniemap/mastermap.js
+++ donniemap/mastermap.js
@@ -7,7 +7,7 @@ var image = L.imageOverlay('/donniemap/assets/map-large.jpg', bounds).addTo(myma
 mymap.fitBounds(bounds);
 var markersById = {};
 
-markersById[1] = L.marker(L.latLng([1800, 1500])).addTo(mymap).bindPopup('<h3><a href="/">Donkey Shows in New Mexico? Not what you think</a></h3><p>Mexico City, NM</p>').openPopup();
+markersById[1] = L.marker(L.latLng([1800, 1500])).addTo(mymap).bindPopup('<h3><a href="/blogs/article-template.html">Donkey Shows in New Mexico? Not what you think</a></h3><p>Mexico City, NM</p>').openPopup();
 markersById[2] = L.marker(L.latLng([1900,1400])).addTo(mymap).bindPopup('<h3><a href="/">Best Burger Joint Before Allegations? GHOSTS INVOLVED!?!?!</a></h3><p>Kalispell, MT</p>').openPopup();
 
 function centerMapOnPost(markerId){
